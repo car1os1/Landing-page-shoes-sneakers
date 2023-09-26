cd23f72368708ca65ccf3d1daec0f3784598f20b
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2023-09-26 15:56:16 -0500

    adding section special ofert

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 
-const MyButton = ({ label, iconURL }) => {
+const MyButton = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
     return (
-        <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-[#F0A500] rounded-md text-[#F4F4F4]">
+        <button
+            className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${backgroundColor
+                    ? `${backgroundColor} ${textColor} ${borderColor}`
+                    : "bg-[#F0A500] text-white border-[#F0A500]"
+                } rounded-full ${fullWidth && "w-full"}`}
+        >
             {label}
             {iconURL && <img src={iconURL} alt="arrow right icon" className=" ml-2 rounded-full w-5 h-5" />}
         </button>
--- src/sections/SpecialOfert.jsx
+++ src/sections/SpecialOfert.jsx
@@ -1,9 +1,37 @@
+import Button from '../components/Button'
+import { offer } from "../assets/images"
+import { arrowRight } from "../assets/icons"
+
+
 
 const SpecialOfert = () => {
     return (
-        <div>
-            <h1>offert</h1>
-        </div>
+        <section className=" flex flex-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+            <div className="flex-1">
+                <img src={offer} width={773} height={687} className=" object-contain w-full" />
+            </div>
+
+
+            <div className=" flex-1 flex flex-col">
+
+                <h2 className=" font-palanquin text-4xl capitalize  font-bold lg:max-w-lg" >
+
+                    <span className="text-[#F0A500] ">Special </span>
+                    Offer
+
+                </h2>
+                <p className=" mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted footewear is designed to elevate your experience , providing you with unmatched queality, innovation, and a touch of elegance. </p>
+
+                <p className=" mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fultfill your unique desires, surpassing the lofttiest expectations. your journey with us is nothing short of exceptional </p>
+
+                <div className="mt-11 flex flex-wrap gap-4">
+                    <Button label="Shop now" iconURL={arrowRight} />
+
+
+                </div>
+
+            </div>
+        </section>
     )
 }
 
